2da69f650f8fea5ceaea6577f3d6bcc578cdef1f
VyasNml <142661775+VyasNml@users.noreply.github.com>
2025-04-08 23:06:26 +0530

    Update P2504_1_Standings_Project.py

--- P2504_1_Standings_Project.py
+++ P2504_1_Standings_Project.py
@@ -7,17 +7,17 @@ import seaborn as sns
 #Reading file
 file = pd.read_csv("C:/Users/vyasn/OneDrive/Desktop/Academics/Project_1/P2504_1_Standings/P2504_1_standings_1.csv")
 
-# print(file.head(),"\n")
+print(file.head(),"\n")
 
 # General info about the dataset
-# print(file.info(),"\n")
+print(file.info(),"\n")
 
 # Check for null values
-# print(file.isnull().sum(),"\n")
+print(file.isnull().sum(),"\n")
 file['year'] = file['year'].fillna(method='ffill')
-# print(file.isnull().sum(),"\n")
+print(file.isnull().sum(),"\n")
 file = file.dropna()
-# print(file.isnull().sum(),"\n")
+print(file.isnull().sum(),"\n")
 
 
 
