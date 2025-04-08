e1a8b618f791b36b886d5640ed6da577c6dd8fcc
VyasNml <142661775+VyasNml@users.noreply.github.com>
2025-04-08 10:24:34 +0530

    Add files via upload

---
+++ P2504_1_Standings_Project.py
@@ -0,0 +1,48 @@
+#import staments
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+#Reading file
+file = pd.read_csv("C:/Users/vyasn/OneDrive/Desktop/Academics/Project_1/P2504_1_Standings/P2504_1_standings_1.csv")
+
+print(file.head(),"\n")
+
+# General info about the dataset
+print(file.info(),"\n")
+
+# Check for null values
+print(file.isnull().sum(),"\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
